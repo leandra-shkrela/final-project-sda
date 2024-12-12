8abf8a4e98b44f1614aee688936f2628c7320f7c
leandraGit <gropajleandra@gmail.com>
2024-12-12 01:33:42 +0100

    header

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Header from "./Header";
 
-function MainPage() {
+export default function MainPage() {
   return (
-    <ul
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <Link to="/">MainPage</Link>
-      <Link to="searchbar">SearchBar</Link>
-      <Link to="cart">ShoppingCart</Link>
-      <Link to="products">AllProducts</Link>
-      <Link to="product/:id">SingleProduct</Link>
-      <Link to="checkout">Checkout</Link>
-      <Link to="contactus">ContactUs</Link>
-    </ul>
+    <div>
+      <Header></Header>
+      <div>MainPage</div>
+    </div>
   );
 }
-
-export default MainPage;
